fca1cc4f48a9d100a53359364cde6b798d6614a7
Yufei <flying3615@163.com>
2019-10-27 13:31:44 +1300

    gallery imgs

--- src/components/menu/GalleryShow.jsx
+++ src/components/menu/GalleryShow.jsx
@@ -1,35 +1,29 @@
 import React, {useState, useCallback} from "react";
 import Gallery from "react-photo-gallery";
 import {graphql, useStaticQuery} from "gatsby";
+import {photos} from 'gallery/photos'
+
 
 export default function GalleryShow(props) {
 
-  const data = useStaticQuery(
-    graphql`
-    query{
-    allMenuCsv {
-      edges {
-        node {
-          src,
-          width,
-          height
-        }
-      }
-    }
-  }`
-  )
+  // TODO how to externalise src ???
+  // const data = useStaticQuery(
+  //   graphql`
+  //   query{
+  //   allMenuCsv {
+  //     edges {
+  //       node {
+  //         src,
+  //         width,
+  //         height
+  //       }
+  //     }
+  //   }
+  // }`
+  // )
 
-  const photos = data && data.allMenuCsv.edges
-    .map(e => ({src: e.node.src, width: e.node.width, height: e.node.height}))
-
-
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, {photo, index}) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
+  // const photos = data && data.allMenuCsv.edges
+  //   .map(e => ({src: e.node.src, width: e.node.width, height: e.node.height}))
 
 
   return (
@@ -42,7 +36,7 @@ export default function GalleryShow(props) {
     >
       <h2 className="major">Gallery</h2>
 
-      <Gallery photos={photos} onClick={openLightbox}/>
+      <Gallery photos={photos} direction={"column"}/>
     </article>
   )
 
