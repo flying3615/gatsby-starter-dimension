35c8e87ac363ff3695a145243b3219152c53ed1c
Yufei <flying3615@163.com>
2019-10-27 13:32:09 +1300

    gallery imgs

---
+++ src/components/menu/gallery/photos.js
@@ -0,0 +1,45 @@
+import iron from "../../../images/gallery/iron.jpeg";
+import chicken from "../../../images/gallery/chicken.jpeg";
+import dumpling from "../../../images/gallery/dumpling.jpeg";
+import spicyMeat from "../../../images/gallery/spicyMeat.jpeg";
+import wontong from "../../../images/gallery/wontong.jpeg";
+import yumcha from "../../../images/gallery/yumcha.jpeg";
+import springroll from "../../../images/gallery/springroll.jpeg";
+
+export const photos = [
+  {
+    src: iron,
+    width: 3,
+    height: 4
+  },
+  {
+    src: chicken,
+    width: 4,
+    height: 3
+  },
+  {
+    src: dumpling,
+    width: 3,
+    height: 4
+  },
+  {
+    src: spicyMeat,
+    width: 4,
+    height: 3
+  },
+  {
+    src: wontong,
+    width: 3,
+    height: 5
+  },
+  {
+    src: yumcha,
+    width: 4,
+    height: 3
+  },
+  {
+    src: springroll,
+    width: 3,
+    height: 4
+  },
+]
