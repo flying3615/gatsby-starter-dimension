9d8015a059d134ee4b97eaa8991220f460412ef6
Yufei <flying3615@163.com>
2019-11-02 14:13:10 +1300

    add chicken

--- src/components/menu/food/main/Chicken.jsx
+++ src/components/menu/food/main/Chicken.jsx
@@ -1,20 +1,34 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Typography from "@material-ui/core/Typography";
+import chicken from "../../../../images/dishes/chicken.jpg";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
 
 const Chicken = ({ data }) => (
-  <ul>
-    {
-      data && data.allChickenCsv.edges
-        .map(e => (
-          <li key={e.node.id}>
-            <Typography variant="subtitle1" gutterBottom>
-              {e.node.NameZh} / {e.node.NameEn} --- ${e.node.price}
-            </Typography>
-          </li>)
-        )
-    }
-  </ul>
+  <div>
+    <Card style={{margin: '15px'}}>
+      <CardMedia
+        component="img"
+        image={chicken}
+      />
+    </Card>
+
+    <ul>
+      {
+        data && data.allChickenCsv.edges
+          .map(e => (
+            <li key={e.node.id}>
+              <Typography variant="subtitle1" gutterBottom>
+                {e.node.NameZh} / {e.node.NameEn} --- ${e.node.price}
+              </Typography>
+            </li>)
+          )
+      }
+    </ul>
+  </div>
+
 )
 export default props => (
   <StaticQuery
