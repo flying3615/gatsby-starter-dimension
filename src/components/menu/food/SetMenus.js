09bba7481b8b429658964f3cb21de0f297a02ca9
Yufei <flying3615@163.com>
2019-12-04 22:26:30 +1300

    refine pic&logo

--- src/components/menu/food/SetMenus.js
+++ src/components/menu/food/SetMenus.js
@@ -1,16 +1,5 @@
 import React, {Component} from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import dish1 from '../../../images/bg_pprich3.jpg'
-import dish2 from '../../../images/bg_pprich2.jpg'
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
 import {Document, Page} from "react-pdf";
 import setMenu from "../../../data/set_menu.pdf";
 import Hidden from "@material-ui/core/Hidden";
@@ -91,43 +80,6 @@ class SetMenus extends Component {
 
           {/*show bottom to show menu*/}
           <Hidden smUp>
-            {/*<Grid item xs={6}>*/}
-              {/*<Card>*/}
-                {/*<CardMedia*/}
-                  {/*component="img"*/}
-                  {/*image={dish1}*/}
-                  {/*title="Contemplative Reptile"*/}
-                {/*/>*/}
-                {/*<CardContent>*/}
-                  {/*<Typography gutterBottom variant="h5" component="h2" color="textSecondary">*/}
-                    {/*Lizard*/}
-                  {/*</Typography>*/}
-                  {/*<Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging*/}
-                    {/*across all continents except Antarctica*/}
-                  {/*</Typography>*/}
-                {/*</CardContent>*/}
-              {/*</Card>*/}
-            {/*</Grid>*/}
-
-            {/*<Grid item xs={6}>*/}
-              {/*<Card>*/}
-                {/*<CardMedia*/}
-                  {/*component="img"*/}
-                  {/*image={dish2}*/}
-                  {/*title="Contemplative Reptile"*/}
-                {/*/>*/}
-                {/*<CardContent>*/}
-                  {/*<Typography gutterBottom variant="h5" component="h2" color="textSecondary">*/}
-                    {/*Lizard*/}
-                  {/*</Typography>*/}
-                  {/*<Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging*/}
-                    {/*across all continents except Antarctica*/}
-                  {/*</Typography>*/}
-                {/*</CardContent>*/}
-              {/*</Card>*/}
-            {/*</Grid>*/}
             <Grid item xs={12} container justify="center">
               <button
                 type="button"
