2162617412e94b51254a7c83cff2740f7925936d
Yufei <flying3615@163.com>
2019-12-08 14:39:46 +1300

    intro

--- src/components/menu/Intro.jsx
+++ src/components/menu/Intro.jsx
@@ -17,24 +17,16 @@ export default class Intro extends Component {
                     <img src={intro} alt=""/>
                 </span>
         <p>
-          Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-          aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-          convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-          magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-          By the way, check out my <a href="#work">awesome work</a>.
+          China Beach Restaurant is a Chinese restaurant with full license and can also serve BYO wine. Our restaurant is located on paraparaumu beach.
+          You can appreciate the charming sea sunset while dining.
+          Our restaurant has been committing to operating a variety of flavors Chinese foods, including Cantgonese-style steam dim sims and barbecued meat,
+          traditional Peking duck and silzzling plate dishes, as well as Sichuan-style poached beef and boiled fish, etc.
+          to provide you with different dining options.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-          libero eu nibh porttitor fermentum. Nullam venenatis erat id
-          vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-          Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-          dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-          lectus. Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. In non lorem sit amet elit
-          placerat maximus. Pellentesque aliquam maximus risus, vel sed
-          vehicula.
+          China Beach Restaurant 是一家拥有全牌照、并可以做BYO Wine的中餐厅。餐厅位于paraparaumu beach海边小镇，您可以在用餐的同时领略到迷人的海上日落。
+          China Beach 一直致力于经营多种口味的中式餐饮，既有广式的点心、各式烧腊，也有传统的北京烤鸭、铁板菜式，同时还提供四川口味的水煮牛、水煮鱼等等，
+          为您提供不同的餐饮选择。
         </p>
       </article>
     )
